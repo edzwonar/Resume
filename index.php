807a49e1e39c07a4f8185a489b612b9eece43387
Emilie <edzwonar@silk2.uvm.edu>
2015-10-09 21:41:41 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Emilie Dzwonar</title>
+        <link rel="stylesheet" href="style.css" type="text/css" media="screen">
+    </head>
+    <body>
+        <section id="contact">
+            <h1 id="name">Emilie Dzwonar</h1>
+            <p id="address">Address: 37 School St., Burlington, VT 05401</p>
+            <a id="phone" href="tel:6102167258">610. 216. 7258.</a> | <a id="email" href="mailto:edzwonar@uvm.edu">edzwonar@uvm.edu</a>
+            
+            <hr>
+        </section>
+        <section id="education">
+            <h2>Education</h2>
+            <h3 class="location">University of Vermont, Burlington, VT</h3>
+            <h4 id="major">Bachelor of Science in Computer Science</h4>
+            <h4>Minor in Mathematics</h4>
+        </section>
+        
+    </body>
+</html>
